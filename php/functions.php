d370c354e52775cf2e6b0dfbe0ddff2ffdea10c2
Nuullll <vfirst218@gmail.com>
2018-04-20 12:05:43 +0800

    Enqueue font css

--- php/functions.php
+++ php/functions.php
@@ -21,3 +21,36 @@ function enqueue_hover_css() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_hover_css' );
+
+/**
+ * enqueue custom font stylesheets
+ */
+function enqueue_font_css() {
+    $font_dir = get_stylesheet_directory_uri() . '/fonts/';
+
+    wp_enqueue_style( 'font-brusher',
+        $font_dir . 'Brusher/stylesheet.css',
+        $ver = wp_get_theme()->get('Version')
+    );
+
+    wp_enqueue_style( 'font-levibrush',
+        $font_dir . 'levibrush/stylesheet.css',
+        $ver = wp_get_theme()->get('Version')
+    );
+
+    wp_enqueue_style( 'font-wc-mano-negra-bta',
+        $font_dir . 'wc-mano-negra-bta/stylesheet.css',
+        $ver = wp_get_theme()->get('Version')
+    );
+
+    wp_enqueue_style( 'font-day-billabong',
+        $font_dir . 'day-billabong/stylesheet.css',
+        $ver = wp_get_theme()->get('Version')
+    );
+
+    wp_enqueue_style( 'font-lucky-turns',
+        $font_dir . 'lucky-turns/stylesheet.css',
+        $ver = wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_font_css' );
